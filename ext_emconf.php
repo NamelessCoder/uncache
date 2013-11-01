ae645203d83e5e9b7602dfb5955a9474a5d143d9
Claus Due <claus@namelesscoder.net>
2013-11-02 00:36:15 +0100

    [TASK] Raise maximum allowed TYPO3 version

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.0.0-6.1.99',
+			'typo3' => '6.0.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
