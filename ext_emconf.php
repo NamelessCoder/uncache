28a9f816110c60b676eadf926fefbf636cfab090
Claus Due <claus@namelesscoder.net>
2014-04-03 02:51:00 +0200

    [TER] 1.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "uncache".
  *
- * Auto generated 11-08-2013 22:33
+ * Auto generated 03-04-2014 02:50
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'You know caches. You hate them while developing - when installed, this extension prevents them all from working. NOT FOR PRODUCTION USE!',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '1.0.1',
+	'version' => '1.1.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -41,7 +41,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:7:{s:12:"ext_icon.gif";s:4:"e922";s:17:"ext_localconf.php";s:4:"6783";s:9:"README.md";s:4:"2f68";s:44:"Classes/Override/Core/Cache/CacheManager.php";s:4:"d823";s:52:"Classes/Override/Core/Cache/Frontend/PhpFrontend.php";s:4:"0f31";s:55:"Classes/Override/Core/Cache/Frontend/StringFrontend.php";s:4:"ebab";s:57:"Classes/Override/Core/Cache/Frontend/VariableFrontend.php";s:4:"898a";}',
+	'_md5_values_when_last_written' => 'a:8:{s:13:"composer.json";s:4:"d6d3";s:12:"ext_icon.gif";s:4:"e922";s:17:"ext_localconf.php";s:4:"d4e8";s:9:"README.md";s:4:"2f68";s:44:"Classes/Override/Core/Cache/CacheManager.php";s:4:"a282";s:52:"Classes/Override/Core/Cache/Frontend/PhpFrontend.php";s:4:"cd58";s:55:"Classes/Override/Core/Cache/Frontend/StringFrontend.php";s:4:"840a";s:57:"Classes/Override/Core/Cache/Frontend/VariableFrontend.php";s:4:"2377";}',
 	'suggests' => array(
 	),
 );
+
+?>
