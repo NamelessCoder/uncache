d6f42d332e3e391eea2aa03e811f7cedb3811571
Alexander Buch <alexander.buch@cartware.de>
2014-04-10 18:50:50 +0200

    Ready for 6.2

---
+++ Classes/Cache/Backend/TransientMemoryBackend.php
@@ -0,0 +1,44 @@
+<?php
+namespace FluidTYPO3\Uncache\Cache\Backend;
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2014 Claus Due <claus@namelesscoder.net>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+
+class TransientMemoryBackend extends \TYPO3\CMS\Core\Cache\Backend\TransientMemoryBackend implements \TYPO3\CMS\Core\Cache\Backend\PhpCapableBackendInterface {
+
+	/**
+	 * @var array Holds php cache entries
+	 */
+	protected $phpEntries = array();
+
+	/**
+	 * Loads PHP code from the cache and require_onces it right away.
+	 *
+	 * @param string $entryIdentifier An identifier which describes the cache entry to load
+	 * @return mixed Potential return value from the include operation
+	 * @api
+	 */
+	public function requireOnce($entryIdentifier) {
+		return isset($this->phpEntries[$entryIdentifier]) ? require_once($this->phpEntries[$entryIdentifier]) : FALSE;
+	}
+}
