7e575e4855ea2996781399e45b8159e7962aa8f9
Claus Due <claus@namelesscoder.net>
2016-11-12 00:45:15 +0100

    [FEATURE] Allow in-memory caching
    Rather than forcibly set NullBackend on everything, performance
    can be slightly better if choosing the TransientMemoryBackend
    for some of the cases which support it.
    
    With this:
    
    * Only PhpCapableBackendInterface gets a NullBackend
    * Any current VariableFrontend gets a TransientMemoryBackend
    * Every other combination gets a VariableFrontend + TransientMemoryBackend

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,19 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-foreach (array_keys($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']) as $configurationName) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$configurationName]['backend']
-        = \TYPO3\CMS\Core\Cache\Backend\NullBackend::class;
+foreach ($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'] as $configurationName => $configuration) {
+    if (is_a($configuration['backend'], \TYPO3\CMS\Core\Cache\Backend\PhpCapableBackendInterface::class, true)) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$configurationName]['backend']
+            = \TYPO3\CMS\Core\Cache\Backend\NullBackend::class;
+    } elseif (is_a($configuration['frontend'], \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class, true)) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$configurationName]['backend']
+            = \TYPO3\CMS\Core\Cache\Backend\TransientMemoryBackend::class;
+        unset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$configurationName]['options']);
+    } else {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$configurationName]['frontend']
+            = \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class;
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$configurationName]['backend']
+            = \TYPO3\CMS\Core\Cache\Backend\TransientMemoryBackend::class;
+        unset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$configurationName]['options']);
+    }
 }
